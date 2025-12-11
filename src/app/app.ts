2abcde3201dbb0bb86b4eb75d71f2bd3d0bec783
Tommy <tommycrabtree@outlook.com>
2025-12-10 23:52:04 -0500

    Updated app ts title

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Rebel');
+  protected readonly title = signal('My Rebel');
 }
