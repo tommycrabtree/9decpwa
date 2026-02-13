572ce510e7868bab169eb4aeeb9074b6a6ef32c4
Tommy <tommycrabtree@outlook.com>
2026-02-13 13:19:03 -0500

    Updated Splash screen

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,20 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    const splash = document.getElementById('initial-splash');
+    const appRoot = document.querySelector('app-root');
+
+    if (appRoot) {
+      appRoot.classList.add('app-ready');
+    }
+
+    if (splash) {
+      splash.classList.add('fade-out');
+
+      setTimeout(() => {
+        splash.remove();
+      }, 700);
+    }
+  })
   .catch((err) => console.error(err));
