3dae83c17825ed1518c777ae5dfcc60ba078bc3b
Tommy Crabtree <tommycrabtree@outlook.com>
2026-07-04 17:25:02 -0700

    Account service hydration per Chat

--- src/core/services/account-service.ts
+++ src/core/services/account-service.ts
@@ -12,6 +12,14 @@ export class AccountService {
   currentUser = signal<User | null>(null);
   private baseUrl = environment.apiUrl;
 
+  constructor() {
+    const user = localStorage.getItem('user');
+    
+    if (user) {
+      this.currentUser.set(JSON.parse(user));
+    }
+  }
+
   register(creds: RegisterCreds) {
     return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
       tap(user => {
@@ -26,8 +34,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
       tap(user => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
       })
     )
